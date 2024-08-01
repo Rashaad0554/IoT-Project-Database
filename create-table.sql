1cc4719993717924e100742c1fedc42f3568358e
Rashaad0554 <112050207+Rashaad0554@users.noreply.github.com>
2024-08-01 15:56:27 -0400

    Update create-table.sql
    Corrected data types

--- create-table.sql
+++ create-table.sql
@@ -1,26 +1,25 @@
 CREATE TABLE `Sensors` (
-  `sensorID` integer,
-  `sensorName` varchar(50),
-  `location` varchar(75),
+  `sensorID` INT,
+  `sensorName` VARCHAR(50),
+  `location` VARCHAR(75),
   PRIMARY KEY (`sensorID`)
 );
 
 CREATE TABLE `Topics` (
-  `topicID` <type>,
-  `sensorID` <type>,
-  `topicName` <type>,
-  `gasName` <type>,
-  `unitOfMeasurement` <type>,
+  `topicID` INT,
+  `sensorID` INT,
+  `topicName` VARCHAR(75),
+  `gasName` VARCHAR(30),
+  `unitOfMeasurement` VARCHAR(20),
   PRIMARY KEY (`topicID`),
   FOREIGN KEY (`sensorID`) REFERENCES `Sensors`(`sensorID`)
 );
 
 CREATE TABLE `Logs` (
-  `logID` integer,
-  `topicID` integer,
-  `measurement` float,
-  `date` date,
-  `time` time,
+  `logID` INT,
+  `topicID` INT,
+  `measurement` FLOAT,
+  `timestamp` TIMESTAMP,
   PRIMARY KEY (`logID`),
   FOREIGN KEY (`topicID`) REFERENCES `Topics`(`topicID`)
 );
